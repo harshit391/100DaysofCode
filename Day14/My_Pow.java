c4a94134fe45adfc43bc4d009bf12e26d214979f
harshit391 <harshitsingla1761@gmail.com>
2023-06-27 17:33:02 +0530

    Day14 Problem3

--- Day14/My_Pow.java
+++ Day14/My_Pow.java
@@ -21,6 +21,8 @@ package Day14;
  * At Last a Special Loop explained below Have a look at that
  */
 
+/*==================================================================================================================================== */
+
 import java.util.Scanner;
 
 class Solution {
@@ -82,3 +84,17 @@ public class My_Pow {
 
     }
 }
+
+/*========================================================================================================= */
+
+// I learnt it while exploring on social Media and This example is provided by ChatGPT
+
+// Explanation of While Loop
+
+// For example, when x = 2.0 and n = 10, the loop would execute as follows:
+
+// Iteration 1: n is even (n % 2 == 0), so no multiplication to result. current is squared (current = 2.0 * 2.0 = 4.0), and n is divided by 2 (n = 10 / 2 = 5).
+// Iteration 2: n is odd (n % 2 == 1), so we multiply result by current (result = 1.0 * 4.0 = 4.0). current is squared (current = 4.0 * 4.0 = 16.0), and n is divided by 2 (n = 5 / 2 = 2).
+// Iteration 3: n is even (n % 2 == 0), so no multiplication to result. current is squared (current = 16.0 * 16.0 = 256.0), and n is divided by 2 (n = 2 / 2 = 1).
+// Iteration 4: n is odd (n % 2 == 1), so we multiply result by current (result = 4.0 * 256.0 = 1024.0). current is squared (current = 256.0 * 256.0 = 65536.0), and n is divided by 2 (n = 1 / 2 = 0).
+// The loop terminates because n becomes 0.
