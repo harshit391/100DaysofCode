1c36ca2cd5503753c4f23ab7f5e21474365f5d01
harshit391 <harshitsingla1761@gmail.com>
2023-07-02 17:32:34 +0530

    Day19 Problem6

--- Day19/Add_Strings.java
+++ Day19/Add_Strings.java
@@ -2,6 +2,38 @@ package Day19;
 
 import java.util.Scanner;
 
+// Leetcode Problem 415
+
+// Add Strings 
+
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+// Example 1:
+
+// Input: num1 = "11", num2 = "123"
+// Output: "134"
+
+// Algorithm
+
+/**
+ * Create a mutable String sb using String Builder
+ * Now Initalize i and j to one less then num1 and num2 length respectively
+ * Initialize a carry int variable to 0
+ * Iterate by while loop till when both i and j become zero completely
+ * Create a sum variable inside loop equal to carry 
+ * Now if i is greater than zero convert the ith character of num1 to number and add it to sum
+ * Similarly is j is greater than zero convert the ith character of num2 to number and add it to sum
+ * Add the result to sb as sum modulus 10 to only get ones place integer like "1" from "21"
+ * Make carry equal sum divided by 10 as we visulize in normal maths
+ * Decrement i and j
+ * At last if any carry remains add it to string sb
+ * Convert the String Builder to String
+ * END
+ */
+
+/* ==================================================================================================================================== */
 class Solution {
     public String addStrings(String num1, String num2) {
 
