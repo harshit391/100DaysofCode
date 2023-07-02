188724c2579dfc1e942732a87370cc090b0e9473
harshit391 <harshitsingla1761@gmail.com>
2023-07-02 09:24:28 +0530

    Day19 Problem1 and Problem2

---
+++ Day19/Fizz_Buzz.java
@@ -0,0 +1,76 @@
+package Day19;
+
+import java.util.*;
+
+// Leetcode Problem 412
+
+// Fizz Buzz
+
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+
+// Algorithm
+
+/*
+ * Create A Array List of Strings
+ * Traverse from 1 to n value
+ * As Per conditions if number divides both 3 and 5 add fizzbuzz to list
+ * Else if number divides 3 only add fizz to list
+ * Number divides only 5 add buzz to list
+ * Else Convert number to string and add it to list of string
+ * END
+ */
+
+ /* ========================================================================================================= */
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+        List<String> sub = new ArrayList<>();
+
+        for (int i=1;i<=n;i++)
+        {
+            if (i%3==0 && i%5==0) sub.add("FizzBuzz");
+
+            else
+            {
+                if (i%3==0) sub.add("Fizz");
+
+                else if (i%5==0) sub.add("Buzz");
+
+                else sub.add(String.valueOf(i));
+            }
+        }
+
+        return sub;
+
+    }
+}
+
+public class Fizz_Buzz {
+    public static void main(String[] args)
+    {
+
+        Scanner scan = new Scanner(System.in);
+
+        int s = scan.nextInt();
+
+        scan.close();
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.fizzBuzz(s));
+
+
+    }
+}
---
+++ Day19/Rotate_Image.java
@@ -0,0 +1,85 @@
+package Day19;
+
+import java.util.*;
+
+// Leetcode Problem 48
+
+// Rotate Image
+
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+// Example 1:
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+// Algorithm
+
+/**
+ * First Transpose the Matrix 
+ * Then Reverse elements in each row
+ * And Its Done... Lol
+ * END
+ */
+
+ /* ============================================================================================== */
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i=0;i<matrix[0].length;i++)
+        {
+            for (int j=0;j<=i;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;   
+            }
+        }
+
+        for (int i=0;i<matrix[0].length;i++)
+        {
+            for (int j=0;j<matrix[0].length/2;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length-j-1];
+                matrix[i][matrix[0].length-j-1] = temp;
+            }
+        }
+
+        
+        
+    }
+}
+
+public class Rotate_Image{
+    public static void main(String []args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter Number of Rows");
+        int row = scan.nextInt();
+
+        System.out.println("Enter Number of Columns");
+        int column = scan.nextInt();
+
+        int[][] matrix = new int[row][column];
+
+        for (int i=0;i<row;i++)
+        {
+            for (int j=0;j<column;j++)
+            {
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+
+        scan.close();
+
+        Solution sol = new Solution();
+
+        sol.rotate(matrix);
+
+
+    }
+}
