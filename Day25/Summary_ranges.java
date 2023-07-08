c30f8fb4b468f63c6aebe01f11ee6cbedc2f8d5e
harshit391 <harshitsingla1761@gmail.com>
2023-07-08 20:19:50 +0530

    Day25 Problem7

--- Day25/Summary_ranges.java
+++ Day25/Summary_ranges.java
@@ -2,21 +2,30 @@ package Day25;
 
 import java.util.*;
 
-// Leetcode Problem 66 
+// Leetcode Problem 228 
 
-// Plus One
+// Summary Ranges
 
-// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+// You are given a sorted unique integer array nums.
 
-// Increment the large integer by one and return the resulting array of digits.
+// A range [a,b] is the set of all integers from a to b (inclusive).
+
+// Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+// Each range [a,b] in the list should be output as:
+
+// "a->b" if a != b
+// "a" if a == b
+ 
 
 // Example 1:
 
-// Input: digits = [1,2,3]
-// Output: [1,2,4]
-// Explanation: The array represents the integer 123.
-// Incrementing by one gives 123 + 1 = 124.
-// Thus, the result should be [1,2,4].
+// Input: nums = [0,1,2,4,5,7]
+// Output: ["0->2","4->5","7"]
+// Explanation: The ranges are:
+// [0,2] --> "0->2"
+// [4,5] --> "4->5"
+// [7,7] --> "7"
 
 // Algorithm
 
