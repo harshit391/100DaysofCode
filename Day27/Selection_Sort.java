0c6417b3bf61e4e5ec439e95c00f5d4d760bdc35
harshit391 <harshitsingla1761@gmail.com>
2023-07-10 12:20:43 +0530

    Selection Sort

--- Day27/Selection_Sort.java
+++ Day27/Selection_Sort.java
@@ -1,5 +1,55 @@
 package Day27;
 
+import java.util.Scanner;
+
 public class Selection_Sort {
+    private static void swap(int a,int b,int[] array)
+    {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    private static void print(int[] array)
+    {
+        for (int a:array) System.out.printf("%d ",a);
+    }
     
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter length of Array:- ");
+        int length = scan.nextInt();
+        System.out.println();
+
+
+        int[] arr = new int[length];
+
+        System.out.println("Enter Array you want to Sort:- ");
+        for (int i=0;i<length;i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+        scan.close();
+        System.out.println();
+
+        System.out.println("Array Before Sorting:- ");
+        print(arr);
+        System.out.println();
+
+        for (int i=0;i<length;i++)
+        {
+            int minmum = i;
+            for (int j=i+1;j<length;j++)
+            {
+                if (arr[j]<arr[minmum]) minmum = j;
+            }
+            swap(i,minmum,arr);
+        }
+
+        System.out.println("Array After Sorting:- ");
+        print(arr);
+        System.out.println();
+    }
 }
