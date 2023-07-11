886c78721755da7dd2636ca636b148fa18b29588
harshit391 <harshitsingla1761@gmail.com>
2023-07-11 17:27:52 +0530

    Day28 Leetcode Problem 912

---
+++ Day28/Sort_an_Array.java
@@ -0,0 +1,24 @@
+package Day28;
+
+// Leetcode Problem 912
+
+// Sort an Array
+
+// Given an array of integers nums, sort the array in ascending order and return it.
+
+// You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+// Example 1:
+
+// Input: nums = [5,2,3,1]
+// Output: [1,2,3,5]
+// Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+
+// Merge Sort :-
+// https://leetcode.com/problems/sort-an-array/solutions/3750014/merge-sort-solution/
+
+
+
+public class Sort_an_Array {
+    
+}
